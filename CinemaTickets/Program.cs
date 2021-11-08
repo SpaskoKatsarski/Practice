ebf2b4ae1f3735bf7e49281587bf5f6688f9d23b
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-11-09 00:54:07 +0200

    Add project files.

---
+++ CinemaTickets/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CinemaTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string film = Console.ReadLine();
+
+            int totalStudent = 0;
+            int totalStandard = 0;
+            int totalKid = 0;
+            int totalTickets = 0;
+
+            while (film != "Finish")
+            {
+                int freePlaces = int.Parse(Console.ReadLine());
+                int totalTicketsForCurrentProjection = 0;
+                for (int i = 0; i < freePlaces; i++)
+                {
+                    string ticketType = Console.ReadLine();
+                    if (ticketType == "End")
+                    {
+                        break;
+                    }
+                    if (ticketType == "student")
+                    {
+                        totalStudent++;
+                    }
+                    else if (ticketType == "standard")
+                    {
+                        totalStandard++;
+                    }
+                    else
+                    {
+                        totalKid++;
+                    }
+                    totalTicketsForCurrentProjection++;
+                    totalTickets++;
+                }
+                double fullynessPercent = (double)totalTicketsForCurrentProjection / freePlaces * 100;
+                Console.WriteLine($"{film} - {fullynessPercent:f2}% full.");
+
+                film = Console.ReadLine();
+            }
+            double percentageStudent = (double)totalStudent / totalTickets * 100;
+            double percentageStandard = (double)totalStandard / totalTickets * 100;
+            double percentageKid = (double)totalKid / totalTickets * 100;
+            Console.WriteLine($"Total tickets: {totalTickets}\n{percentageStudent:f2}% student tickets.\n{percentageStandard:f2}% standard tickets.\n{percentageKid:f2}% kids tickets.");
+        }
+    }
+}
---
+++ SumPrimeNonPrime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int totalPrime = 0;
+            int totalNonPrime = 0;
+
+            while (input != "stop")
+            {
+                int currentNumber = int.Parse(input);
+
+                if (currentNumber < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    int counter = 0;
+                    for (int i = 1; i <= currentNumber; i++)
+                    {
+                        if (currentNumber % i == 0)
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 2)
+                    {
+                        totalPrime += currentNumber;
+                    }
+                    else
+                    {
+                        totalNonPrime += currentNumber;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {totalPrime}\nSum of all non prime numbers is: {totalNonPrime}");
+        }
+    }
+}
