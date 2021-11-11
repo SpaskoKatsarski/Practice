4630c63a0c5d8d487652e1b95464c6ab688b34a4
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-11-11 15:14:12 +0200

    Updated

---
+++ SumEvenAndOddNums/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SumEvenAndOddNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int sumOddNums = 0;
+            int sumEvenNums = 0;
+
+            while (input != "Stop")
+            {
+                int currentNumber = int.Parse(input);
+                int counter = 0;
+                for (int i = 1; i <= currentNumber; i++)
+                {
+                    if (currentNumber % i == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 2)
+                {
+                    sumEvenNums += currentNumber;
+                }
+                else
+                {
+                    sumOddNums += currentNumber;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Odd sum: {sumOddNums}\nEven Sum: {sumEvenNums}");
+        }
+    }
+}
