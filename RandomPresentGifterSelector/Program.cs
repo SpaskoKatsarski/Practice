7f26db7095de75c65bc39fcb8fad3ed6520c0dec
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-11-22 18:59:54 +0200

    Trying new stuff

---
+++ RandomPresentGifterSelector/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomPresentGifterSelector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 1; i < 28; i++)
+            {
+                Console.WriteLine("Please enter a number:");
+
+                int input = int.Parse(Console.ReadLine());
+                Random rnd = new Random();
+
+                int number1 = rnd.Next(1, 27);
+
+                if (list) // Make that so if a number from the randoms has been already used, the loop breaks and generates another number.
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Number {input} will gift a present to number {number1}");
+
+                list.Add(number1);
+            }
+        }
+    }
+}
