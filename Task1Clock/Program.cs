33fc5c63c722c40f29ba25033c60af99e7aabdeb
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-12-03 18:37:12 +0200

    Updated

--- Task1Clock/Program.cs
+++ Task1Clock/Program.cs
@@ -6,13 +6,9 @@ namespace Task1Clock
     {
         static void Main(string[] args)
         {
-            for (int hours = 0; hours < 24; hours++)
-            {
-                for (int minutes = 0; minutes < 60; minutes++)
-                {
-                    Console.WriteLine($"{hours:d2}:{minutes:d2}");
-                }
-            }
+            string str = Console.ReadLine();
+            Console.WriteLine("The string of '{0}' is {1}", str, str.Length);
+            Console.WriteLine("The length of '{0}' is {1}", "xyasd", "xyasd".Length);
         }
     }
 }
