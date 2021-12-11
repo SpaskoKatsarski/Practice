40e9d12aa3d4f8f3d471ce9ffb7d8f99b1e72996
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-12-11 09:35:27 +0200

    Updated

---
+++ DifferentAges/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DifferentAges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            string result = age <= 2 ? "baby" : age <= 19 ? "teenager" : age <= 50 ? "adult" : age > 50 ? "older than fifty" : "older";
+
+            Console.WriteLine(result);
+        }
+    }
+}
