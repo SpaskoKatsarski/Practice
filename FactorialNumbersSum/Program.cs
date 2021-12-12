35a1a4b2a241d1824ab7f2a84d54f42fb6f22413
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-12-12 09:02:00 +0200

    Updated

---
+++ FactorialNumbersSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FactorialNumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int temporaryNumber = number;
+            int factorialSum = 9;
+
+            while (temporaryNumber > 0)
+            {
+                int currentNumber = temporaryNumber % 10;
+                temporaryNumber /= 10;
+                int currentFactNum = 1;
+
+                for (int i = 1; i <= currentFactNum; i++)
+                {
+                    currentFactNum *= i;
+                }
+                factorialSum += currentFactNum;
+            }
+            if (factorialSum == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
---
+++ PasswordBackwards/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordBackwards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = new string(username.ToCharArray().Reverse().ToArray());
+
+            string input = Console.ReadLine();
+
+            int count = 0;
+
+            while (input != password)
+            {
+                count++;
+
+                if (count == 4)
+                {
+                    Console.WriteLine("User blocked!");
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Hello {username}!");
+
+        }
+    }
+}
