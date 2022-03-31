794f92554eeabce8be7279051194ce2d561a8925
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-31 20:15:44 +0300

    Updated

---
+++ ShowingStuffToFriend/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShowingStuffToFriend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User, please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("User, please enter your age:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{name}, what's your grade?");
+            double studentGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"You are {name}, {age} age years old and your grade is {studentGrade:f2}");
+        }
+    }
+}
